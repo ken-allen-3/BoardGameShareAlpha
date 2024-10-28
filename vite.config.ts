94a410e93fbaac30d6f15f79e6536df95ec4c6d6
Kenny Allen <kenny@springwavestudios.com>
2024-10-27 23:43:40 -0700

    Troubleshooting last commit date by updating vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/lastCommitDate.json',
+          dest: ''
+        }
+      ]
+    })
+  ],
   server: {
     host: '192.168.1.3',
     port: 5173,
