67420d350171a65c0abd11f4d45c9da3045b2ddc
Kenny Allen <kenny@springwavestudios.com>
2024-10-27 23:36:24 -0700

    Converted getLastCommitDate script to ES module syntax

--- getLastCommitDate.js
+++ getLastCommitDate.js
@@ -1,11 +1,11 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 try {
   const lastCommitDate = execSync('git log -1 --format=%cd').toString().trim();
-  const filePath = path.join(__dirname, 'src', 'lastCommitDate.json');
-  fs.writeFileSync(filePath, JSON.stringify({ lastCommitDate }));
+  const filePath = join(process.cwd(), 'src', 'lastCommitDate.json');
+  writeFileSync(filePath, JSON.stringify({ lastCommitDate }));
   console.log('Last commit date written to src/lastCommitDate.json');
 } catch (error) {
   console.error('Error getting last commit date:', error);
